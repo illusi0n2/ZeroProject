5e2ccfd69084928745ce98d08bb81c1013866fc7
illusi0n2 <graf26.91@gmail.com>
2025-06-15 20:53:58 +0300

    Upgreded my TG bot
    Доработал кнопки взаимодействия с ботом

--- ZeroProject/ZeroProject/OP_09/tg_bot.py
+++ ZeroProject/ZeroProject/OP_09/tg_bot.py
@@ -1,10 +1,10 @@
 import time
 import telebot
+from telebot import types
 from datetime import datetime
 from task import Task
 import threading
 import re
-
 
 bot = telebot.TeleBot('TOKEN')
 tasks_list = [Task('123', '28-05-2025 12:50'), Task('123', '28-05-2025 12:55'), Task('fdsfsdf', '28-05-2025 12:50'), Task('utyutyutyu', '28-05-2025 12:50')]
@@ -12,9 +12,37 @@ matches = []
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, 'Привет!')
+    bot.reply_to(message, 'Привет! Я SmartReminder Bot - ваш персональный помощник для управления задачами и '
+                          'напоминаний в Telegram. Создавайте задачи и устанавливайте время напоминания прямо в мессенджере. '
+                          'Я отправлю уведомление точно в указанный момент, чтобы вы ничего не забыли.')
     tread = threading.Thread(target=send_reminder, args=(message.chat.id,))
     tread.start()
+    send_welcome(message)
+
+@bot.message_handler(commands=['start1'])
+def send_welcome(message):
+    # Создаем клавиатуру
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    # Добавляем кнопки
+    keyboard.row(types.KeyboardButton('➕️ Создать новую задачу'))
+    keyboard.row(types.KeyboardButton('📋 Посмотреть список задач'))
+    keyboard.row(types.KeyboardButton('⛔ Удалить задачу'))
+    keyboard.row(types.KeyboardButton('Обратно в меню'))
+
+    # Отправляем сообщение с клавиатурой
+    bot.send_message(message.chat.id, "Выбери интересующее действие:", reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: True)
+def handle_buttons(message):
+    if message.text.startswith('➕️'):
+        create_task(message)
+    elif message.text.startswith('📋'):
+        show_all_tasks(message)
+    elif message.text.startswith('⛔'):
+        delete_message(message)
+    # elif message.text == 'Обратно в меню':
+    #     send_welcome(message)  # Возвращаемся к начальной клавиатуре
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
@@ -28,17 +56,18 @@ def create_task(message):
 
 @bot.message_handler(commands=['tasks_list'])
 def show_all_tasks(message):
-    msg = ""
+    msg = "Вот список задач, о которых ты просил напомнить: \n"
     for i in range(len(tasks_list)):
         msg += f'Напоминание о {tasks_list[i].get_name()} установлено на {tasks_list[i].get_date()} \r'
-    bot.reply_to(message, msg)
+    if msg == "Вот список задач, о которых ты просил напомнить: \n":
+        bot.reply_to(message, 'Нет задач, о которых нужно напомнить')
+    else:
+        bot.reply_to(message, msg)
 
 @bot.message_handler(commands=['delete_task'])
 def delete_message(message):
     msg = bot.reply_to(message, "Введи название задачи, которую хочешь удалить")
     bot.register_next_step_handler(msg, chek_task)
-
-
 
 def task_name_step(message):
     try:
@@ -64,6 +93,7 @@ def task_date_step(message, task_name):
                 bot.reply_to(message, f'Напоминание установлено на {date}')
                 created_task = Task(task_name, date)
                 tasks_list.append(created_task)
+                send_welcome(message)
             else:
                 bot.reply_to(message, f'Даты {date} не существует. Попробуй еще раз')
                 bot.register_next_step_handler(message, task_date_step)
@@ -89,6 +119,7 @@ def chek_task(message):
         del tasks_list[matches[0]]
         bot.reply_to(message, 'Напоминание удалено')
         matches.clear()
+        send_welcome(message)
 def delete_task(message, index):
     try:
         i = int(message.text)
@@ -98,6 +129,7 @@ def delete_task(message, index):
         bot.reply_to(message, "Произошла ошибка")
         print(f"Ошибка: {e}")
     matches.clear()
+    send_welcome(message)
 
 def validate_datetime(date_string):
     pattern = re.compile(r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$')
